e4e66c617a4868cedc84733bf5c0c289d013e498
vikkont79 <vvikkont79@yandex.ru>
2025-04-21 18:55:27 +0300

    Удалил временные файлы, исправил ошибку линтера

--- src/components/main-index/main-index.tsx
+++ src/components/main-index/main-index.tsx
@@ -1,39 +1,26 @@
-import { Locations } from '../../components/locations/locations';
 import { PlacesSorting } from '../../components/places-sorting/places-sorting';
 import { PlacesList } from '../../components/places-list/places-list';
 import { PlaceCardProps } from '../../mocks/mocks';
-import { useState } from 'react';
 
 type MainIndexProps = {
-  placeCards: PlaceCardProps[];
+  cityPlaceCards: PlaceCardProps[];
+  cityName: string;
 }
 
-function MainIndex({ placeCards }: MainIndexProps): JSX.Element {
-  const [cityName, setCityName] = useState<string>('Paris');
-  const handleCityChange = (newCityName: string) => {
-    setCityName(newCityName);
-  };
-
-  const cityPlaceCards = placeCards.filter((place) => place.city?.name === cityName);
-
+function MainIndex({ cityPlaceCards, cityName }: MainIndexProps): JSX.Element {
   return (
-    <main className="page__main page__main--index">
-      <h1 className="visually-hidden">Cities</h1>
-      <Locations onCityChange={handleCityChange} />
-      <div className="cities">
-        <div className="cities__places-container container">
-          <section className="cities__places places">
-            <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">{cityPlaceCards.length} places to stay in {cityName}</b>
-            <PlacesSorting />
-            <PlacesList placeCards={cityPlaceCards} />
-          </section>
-          <div className="cities__right-section">
-            <section className="cities__map map"></section>
-          </div>
-        </div>
+    <div className="cities__places-container container">
+      <section className="cities__places places">
+        <h2 className="visually-hidden">Places</h2>
+        <b className="places__found">{cityPlaceCards.length} places to stay in {cityName}</b>
+        <PlacesSorting />
+        <PlacesList placeCards={cityPlaceCards} />
+      </section>
+      <div className="cities__right-section">
+        <section className="cities__map map"></section>
       </div>
-    </main>
+    </div>
+
   );
 }
 
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,8 +1,11 @@
 import { Header } from '../../components/header/header';
+import { Locations } from '../../components/locations/locations';
 import { MainIndex } from '../../components/main-index/main-index';
 import { MainIndexEmpty } from '../../components/main-index/main-index-empty';
 import { PlaceCardProps } from '../../mocks/mocks';
 import { AuthorizationStatus } from '../../const';
+import { useState } from 'react';
+
 
 type MainPageProps = {
   placeCards: PlaceCardProps[];
@@ -10,11 +13,29 @@ type MainPageProps = {
 }
 
 function MainPage({placeCards, authStatus}: MainPageProps): JSX.Element {
+  const [cityName, setCityName] = useState<string>('Paris');
+
+  const handleCityChange = (newCityName: string) => {
+    setCityName(newCityName);
+  };
+
+  const cityPlaceCards = placeCards.filter((place) => place.city?.name === cityName);
+
+  const mainClass = cityPlaceCards.length > 0
+    ? 'page__main page__main--index'
+    : 'page__main page__main--index page__main--index-empty';
+
   return (
     <div className="page page--gray page--main">
       <Header authStatus={authStatus} />
 
-      {placeCards && placeCards.length > 0 ? <MainIndex placeCards={placeCards}/> : <MainIndexEmpty />}
+      <main className={mainClass}>
+        <h1 className="visually-hidden">Cities</h1>
+        <Locations onCityChange={handleCityChange} />
+        <div className="cities">
+          {cityPlaceCards && cityPlaceCards.length > 0 ? <MainIndex cityPlaceCards={cityPlaceCards} cityName={cityName} /> : <MainIndexEmpty />}
+        </div>
+      </main>
 
     </div>
   );
